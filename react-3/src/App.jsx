b481ab8d2be3fc57b59ffb28e8bfd641b0e89e5a
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-03-22 18:10:19 +0530

    state

---
+++ react-3/src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const App = () => {
+
+
+// const [state, setstate] = useState(0);
+
+// let increase = async()=>{
+//    setstate(state + 1)
+//    console.log(state)
+// }
+
+// let decrease = ()=>{
+//   setstate(state-1)
+  
+// }
+// let jumping =() =>{
+//   setstate(state+5)
+// }
+
+  return (
+    <div>
+
+      <form action="">
+        <input type="text" name="name" id="" placeholder='name'/>
+        <button>Submit</button>
+
+      </form>
+      {/* <h1> {state}</h1>
+      <button onClick={increase}>inc</button>
+      <button onClick={decrease}>dec</button>
+      <button onClick={jumping}>JUMP5</button> */}
+    </div>
+  )
+}
+
+export default App
